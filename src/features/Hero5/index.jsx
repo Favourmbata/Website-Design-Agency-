eab14a400637296b105ad2dea3eec344cd83ff10
favourmbata <favourmbata520@gmail.com>
2024-03-17 19:01:47 +0100

    added another functonality

--- src/features/Hero5/index.jsx
+++ src/features/Hero5/index.jsx
@@ -7,80 +7,83 @@ import icon4 from "../../assert/Icon.png"
 
 const Hero5 = ()=>{
     return(
-        <div>
+        <div className={style.general}>
             <div className={style.mainCont}>
-
-                <div className={style.secondCont}>
-                    <h4>Helping a local business <span
-                        style={{color: "#66BA6A", display: "block"}}>reinvent itself</span></h4>
-                </div>
-            </div>
-
-            <div className={style.thirdCont}>
-                <p>We reached here with our hard work and dedication</p>
-            </div>
-
-
-
-            <div className={style.description}>
-
-                <div className={style.group1}>
-
-                    <div>
-                    <img src={icon1} alt={""} className={style.iconPic}/>
+                <div>
+                    <div className={style.secondCont}>
+                        <h4>Helping a local business <span
+                            style={{color: "#66BA6A", display: "block"}}>reinvent itself</span></h4>
                     </div>
 
-                        <div>
-                        <h4>2,245,341</h4>
-                       </div>
-
-                    <div>
-                        <p className={style.members}>Members</p>
+                    <div className={style.thirdCont}>
+                        <p>We reached here with our hard work and dedication</p>
                     </div>
-
-
-                    <div>
-                        <img src={icon2} alt={""}/>
-                    </div>
-
-                    <div>
-                        <h4>46,328</h4>
-                    </div>
-                    <p className={style.club}>Clubs</p>
 
                 </div>
 
+                <div className={style.majorDiv}>
+                    <div className={style.description}>
 
-                <div className={style.group2}>
+                        <div className={style.group1}>
+                            <div>
+                                <img src={icon1} alt={""} className={style.iconPic}/>
+                            </div>
 
-            <div>
-                <img src={icon3} alt={""}/>
+                            <div>
+                                <h4>2,245,341</h4>
+                                <p>Members</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className={style.group2}>
+                            <div>
+                                <img src={icon2} alt={""} className={style.iconPic2}/>
+                            </div>
+
+                            <div>
+                                <h4>46,328</h4>
+                                <p className={style.club}>Clubs</p>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className={style.description1}>
+
+                        <div className={style.group3}>
+                            <div>
+                                <img src={icon3} alt={""} className={style.iconPic3}/>
+                            </div>
+                            <div>
+                                <h4> 828,867</h4>
+                                <p>Event Booking</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className={style.group4}>
+
+                            <div>
+                                <img src={icon4} alt={""} className={style.iconPic4}/>
+                            </div>
+
+
+                            <div>
+                                <h4>1,926,436</h4>
+                                <p className={style.pay}>Payments</p>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
-
-            <div>
-                <h4>828,867</h4>
-            </div>
-            <p className={style.book}>Event Bookings</p>
-
-
-            <div>
-                <img src={icon4} alt={""}/>
-            </div>
-
-            <div>
-                <h4>1,926,436</h4>
-            </div>
-            <p className={style.pay}>Payment</p>
-
-
-
-
-              </div>
-
-
-
-            </div>
-
 
 
         </div>
---
+++ src/features/Hero6/index.jsx
@@ -0,0 +1,5 @@
+import style from "./index.module.css"
+
+const  Hero6 = ()=>{
+
+}
